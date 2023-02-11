b2c1ba6500ab78e35bd49782080ddafea7024857
Leandro Paiva <leandroapaiva@yahoo.com>
2023-02-11 13:17:20 -0300

    ✔ feat: 9 - Desenvolva o endpoint /login no backend de maneira ele não permita o acesso com um email inválido no frontend

--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -9,9 +9,12 @@ export interface IuserService {
 export default class LoginService {
   private user = Users;
   login = async (email: string, password: string): Promise<IuserService> => {
+    // const regex = /^.@.\.com$/;
     if (!email || !password) return ({ cod: 400, inf: { message: 'All fields must be filled' } });
 
     const getUser = await this.user.findOne({ where: { email } }) as Users;
+    if (!getUser) return ({ cod: 401, inf: { message: 'Incorrect email or password' } });
+
     const token = genToken(getUser.email);
     return { cod: 200, inf: { token } };
   };
