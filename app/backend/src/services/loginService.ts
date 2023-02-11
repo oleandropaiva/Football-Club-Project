93e6dbe0d40762c91f617b1761f16ffcd533faa6
Leandro Paiva <leandroapaiva@yahoo.com>
2023-02-11 12:49:48 -0300

    ✔ feat: 7 - Desenvolva o endpoint /login no backend de maneira ele não permita o acesso sem informar uma senha no frontend

--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -8,8 +8,8 @@ export interface IuserService {
 
 export default class LoginService {
   private user = Users;
-  login = async (email: string, _password: string): Promise<IuserService> => {
-    if (!email) return ({ cod: 400, inf: { message: 'All fields must be filled' } });
+  login = async (email: string, password: string): Promise<IuserService> => {
+    if (!email || !password) return ({ cod: 400, inf: { message: 'All fields must be filled' } });
 
     const getUser = await this.user.findOne({ where: { email } }) as Users;
     const token = genToken(getUser.email);
