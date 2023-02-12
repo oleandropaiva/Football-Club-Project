168170eee142ffd0d05b54df544c127e5499027d
Leandro Paiva <leandroapaiva@yahoo.com>
2023-02-12 14:27:37 -0300

    ✔ feat: Cria metodo matchesInProgress filtra partidas finalizadas e partidas em andamento

--- app/backend/src/controllers/matchesControllers.ts
+++ app/backend/src/controllers/matchesControllers.ts
@@ -5,6 +5,13 @@ export default class MatchesController {
   private matchesService = new MatchesService();
 
   getMatches = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+    if (inProgress) {
+      const { cod, inf } = await this.matchesService.matchesInProgress(
+        inProgress as string,
+      );
+      return res.status(cod).json(inf);
+    }
     const { cod, inf } = await this.matchesService.getMatches();
     return res.status(cod).json(inf);
   };
--- app/backend/src/services/matchesService.ts
+++ app/backend/src/services/matchesService.ts
@@ -15,4 +15,17 @@ export default class MatchService {
 
     return { cod: 200, inf: allMatches };
   };
+
+  matchesInProgress = async (query: string): Promise<IService> => {
+    const isTrue = query === 'true';
+    const allMatchesInProgress = await this.matches.findAll({
+      where: { inProgress: isTrue },
+      include: [
+        { model: Teams, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: Teams, as: 'awayTeam', attributes: { exclude: ['id'] } },
+      ],
+    }) as Matches[];
+
+    return { cod: 200, inf: allMatchesInProgress };
+  };
 }
