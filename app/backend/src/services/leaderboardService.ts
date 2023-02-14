2e81d7f805b887af27f45a15fb192f372280d526
Leandro Paiva <leandroapaiva@yahoo.com>
2023-02-14 00:09:30 -0300

    ✔ feat: Cria o endpoint /leaderboard/home

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -7,7 +7,16 @@ export default class Leaderboard {
 
   getLeaderboard = async (): Promise<IService> => {
     const [allMatches] = await this.matches
-      .sequelize?.query(querys.homeTeam) as ILeaderboard[];
-    return { cod: 200, inf: allMatches };
+      .sequelize?.query(querys.homeTeam) as [ILeaderboard[], unknown];
+
+    const table = this.tableMatches(allMatches);
+    return { cod: 200, inf: table };
   };
+
+  tableMatches = (allMatches: ILeaderboard[]) =>
+    allMatches.map((e) => ({ ...e,
+      efficiency: ((e.totalPoints / (e.totalGames * 3)) * 100).toFixed(2),
+    }));
 }
+
+// [P / (J * 3)] * 100
--- app/backend/src/services/query.ts
+++ app/backend/src/services/query.ts
@@ -2,20 +2,20 @@ const matches = 'TRYBE_FUTEBOL_CLUBE.matches';
 const teams = 'TRYBE_FUTEBOL_CLUBE.teams';
 
 const homeTeam = `SELECT
-T.team_name AS name
+T.team_name AS name,
 SUM(
   CASE
     WHEN M.home_team_goals > M.away_team_goals THEN 3
     WHEN M.home_team_goals = M.away_team_goals THEN 1
     WHEN M.home_team_goals < M.away_team_goals THEN 0
   END
-) AS totalPoints
-  COUNT(M.home_team_id) AS totalGames
-  SUM(M.home_team_goals > M.away_team_goals) AS totalVictories
-  SUM(M.home_team_goals = M.away_team_goals) AS totalDraws
-  SUM(M.home_team_goals < M.away_team_goals) AS totalLosses
-  SUM(M.home_team_goals) AS goalsFavor
-  SUM(M.away_team_goals) AS goalsOwn
+) AS totalPoints,
+  COUNT(M.home_team_id) AS totalGames,
+  SUM(M.home_team_goals > M.away_team_goals) AS totalVictories,
+  SUM(M.home_team_goals = M.away_team_goals) AS totalDraws,
+  SUM(M.home_team_goals < M.away_team_goals) AS totalLosses,
+  SUM(M.home_team_goals) AS goalsFavor,
+  SUM(M.away_team_goals) AS goalsOwn,
   SUM(M.home_team_goals - M.away_team_goals) AS goalsBalance
 FROM ${matches} AS M
 INNER JOIN ${teams} AS T
